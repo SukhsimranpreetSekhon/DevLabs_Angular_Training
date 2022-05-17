fdeeea24f97c6c8a2a568e4eb5a263c930a1a2f3
Sukhsimranpreet Sekhon <sukhu57@gmail.com>
2022-05-17 17:40:22 +0000

    [New] Added Aaron_HeroApp & Sukhu_ForecastApp Angular Experiments

---
+++ Aaron_HeroApp/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 20, name: 'Batman'},
+    {id: 15, name: 'Cyborg'},
+    {id: 77, name: 'Catwoman'},
+    {id: 9, name: 'Robin'},
+    {id: 1, name: 'Superman'},
+    {id: 2, name: 'Flash'},
+    {id: 99, name: 'Green Lantern'},
+    {id: 10, name: 'Wonder Woman'},
+    {id: 21, name: 'Alfred'},
+]
